d104cd8f23b25b6dab6aa1b593ef809abc38a83b
Arunoda Susiripal <arunoda.susiripala@gmail.com>
2021-12-18 19:51:08 +0530

    Add homepage

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="bg-gray-900 h-screen flex justify-center items-center">
+      <div className="text-center">
+        <div className="text-gray-100 text-6xl font-black">
+          youfool.art
+        </div>
+        <div className="text-gray-200 mt-4 tracking-wide">The Simplest NFT Platform Ever</div>
+      </div>
+      <div className="text-center fixed bottom-10 text-gray-400">
+        Follow <a className="border-b" href="https://twitter.com/arunoda">@arunoda</a> for updates
+      </div>
+    </div>
   )
 }
